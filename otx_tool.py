0f455182dcd80fc93ce653021b79c926688ff236
Drew <agipson@csus.edu>
2018-11-16 23:40:03 -0800

    switch options support from 'getopt' library to 'argparse' library

--- otx_tool.py
+++ otx_tool.py
@@ -1,53 +1,80 @@
 from secrets import otxKey
 from OTXv2 import OTXv2
-from pandas.io.json import json_normalize
-from datetime import datetime, timedelta
-import getopt
+# from pandas.io.json import json_normalize
+# from datetime import datetime, timedelta
+import argparse
 import sys
-from sendemail import sendemail
+# from sendemail import sendemail
 
-time = str(datetime.now() - timedelta()) # Time at which script was initiated.
+# time = str(datetime.now() - timedelta()) # Time at which script was initiated.
 
-try: # Handles user input options.
-    opts = getopt.getopt(sys.argv[1:],"hdtmiavro")
-    opts = opts[1][0:]
-    options = []
-    for opt in opts:
-        if opt=='h':
-            print("\n"+"AVAILABLE OPTIONS: s:search h:help d:description t:tags m:modified i:id a:author_name \nv:adversary r:references o:revisions\n\ni.e: python3 otx_blkchn.py d n r")
-            print()
-            sys.exit()
-        elif opt=='d':
-            options.append('description')
-        elif opt=='s':
-            userSearch = str(input('Please enter search query: '))
-            userSearch.strip()
-        elif opt=='t':
-            options.append('tags')
-        elif opt=='m':
-            options.append('modified')
-        elif opt=='i':
-            options.append('id')
-        elif opt=='a':
-            options.append('author_name')
-        elif opt=='v':
-            options.append('adversary')
-        elif opt=='r':
-            options.append('references')
-        elif opt=='o':
-            options.append('revisions')
-except getopt.GetoptError:
-    print("\n"+"python3 otx_blkchn.py h for help"+"\n")
-    sys.exit()
+args = argparse.ArgumentParser()
 
-otx = OTXv2(otxKey) # Initializes session with OTXv2 API using key contained in secrets.py
+args.add_argument('-d', '--description',
+                    const='description',
+                    action='append_const',  
+                    dest='optList',
+                    default=[],
+                    help='returns description of each pulse')
+args.add_argument('-t', 'tags',
+                    const='tags',
+                    action='append_const',
+                    dest='optList',
+                    default=[],
+                    help='returns tags for each pulse')
+args.add_argument('-m', 'modified',
+                    const='modified',
+                    action='append_const',
+                    dest='optList',
+                    default=[],
+                    help='return date modified for each pulse')
+args.add_argument('-i', 'id',
+                    const='id',
+                    action='append_const',
+                    dest='optList',
+                    default=[],
+                    help='return pulse ids')
+args.add_argument('-au', 'author_name',
+                    const='author_name',
+                    action='append_const',
+                    dest='optList',
+                    default=[],
+                    help='return author\'s name for each pulse') 
+args.add_argument('-ad', 'adversary',
+                    const='adversary',
+                    action='append_const',
+                    dest='optList',
+                    default=[],
+                    help='return adversary title for pulses')
+args.add_argument('-ref', 'references',
+                    const='references',
+                    action='append_const',
+                    dest='optList',
+                    default=[],
+                    help='return each pulses references')
+args.add_argument('-rev', 'revisions',
+                    const='revisions',
+                    action='append_const',
+                    dest='optList',
+                    default=[],
+                    help='return pulse revisions')
+args.add_argument('search',
+                    metavar='S',
+                    type=str,
+                    help='list search term',
+                    default='crypto')
 
-if 's' in opts: # Handles 'search' option, with 'crypto' as the default search query
-    x = userSearch
-else:
-    x = "crypto"
+options = args.parse_args()
 
-pulses = otx.search_pulses(x, 40) # Retrieves list (in json format) of top 40 pulses with tag "crypto"
+try:
+    otx = OTXv2(otxKey) # Initializes session with OTXv2 API using key contained in secrets.py
+except: 
+    print("problem with API key")
+
+pulses = otx.search_pulses(options.search, 40) # Retrieves list (in json format) of top 40 pulses with tag "crypto"
+    
+
+'''
 
 def pulse_print(): # Loops through each individual pulse retrieved from OTX, and prints name & requested fields.
     for aPulse in pulses["results"]:
@@ -62,8 +89,5 @@ def pulse_print(): # Loops through each individual pulse retrieved from OTX, and
     print()
     print("\n"+"AVAILABLE OPTIONS: s:search h:help d:description t:tags m:modified i:id a:author_name \nv:adversary r:references o:revisions\n\ni.e: python3 otx_blkchn.py d n r")
     print()
-   
-print()
-print("DATE AND TIME: " + time + "\n")
-print('-'*40)
-print()
+
+    '''
